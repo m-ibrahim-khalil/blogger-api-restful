bf20a788cd9997d87e509fda856a033a34ee977c
bsse1009 <mik858692@gmail.com>
2023-05-11 11:18:38 +0600

    modified env varibles

--- config/configuration.js
+++ config/configuration.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 const development = {
   env: "dev",
-  port : process.envDemo.PORT || 3000,
-  host: process.envDemo.HOST
+  port : process.env.PORT || 3000,
+  host: process.env.HOST || 'localhost'
 }
 
 const test = {}
 const production = {}
 const dbConfig = {
-  user: process.envDemo.DB_USER,
-  host: process.envDemo.DB_HOST,
-  database: process.envDemo.DB_NAME,
-  password: process.envDemo.DB_PASSWORD,
-  port: process.envDemo.DB_PORT,
+  user: process.env.DB_USER || 'liltlrmi',
+  host: process.env.DB_HOST || 'arjuna.db.elephantsql.com',
+  database: process.env.DB_NAME || 'liltlrmi',
+  password: process.env.DB_PASSWORD || 'qxQoXwazc7as3gxRATxJeM2ga1J66wgP',
+  port: process.env.DB_PORT || 5432,
   dialect: 'postgres'
 }
 
