50234f5aa3036f2ec4682125a5eb8c14ab684195
bsse1009 <mik858692@gmail.com>
2023-05-11 09:41:00 +0600

    hosting pg in a server

--- config/configuration.js
+++ config/configuration.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 const development = {
   env: "dev",
-  port : process.env.PORT || 3000,
-  host: process.env.HOST
+  port : process.envDemo.PORT || 3000,
+  host: process.envDemo.HOST
 }
 
 const test = {}
 const production = {}
 const dbConfig = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.envDemo.DB_USER,
+  host: process.envDemo.DB_HOST,
+  database: process.envDemo.DB_NAME,
+  password: process.envDemo.DB_PASSWORD,
+  port: process.envDemo.DB_PORT,
   dialect: 'postgres'
 }
 
